88b7834d6cc28652e6c2076673b844e7f98e19f4
Artur Gontijo <arturgontijo@gmail.com>
2021-07-21 08:03:39 -0300

    [ci] Using circleCI orb.

--- run_service.py
+++ run_service.py
@@ -8,12 +8,11 @@ import pathlib
 import glob
 import json
 import argparse
-import service
+
 from service import registry
 
 logging.basicConfig(level=10, format="%(asctime)s - [%(levelname)8s] - %(name)s - %(message)s")
 log = logging.getLogger("run_causality_detection_service")
-
 
 
 def main():
@@ -49,9 +48,9 @@ def start_all_services(cwd, service_modules, run_daemon, run_ssl):
     snetd will start with configs from "snetd.config.json"
     """
     all_p = []
-    for i, service_module in enumerate(service_modules):
+    for _, service_module in enumerate(service_modules):
         service_name = service_module.split(".")[-1]
-        log.info("Launching {} on port {}".format(service_module, str(registry[service_name])))
+        log.info("Launching {} on port {}".format(str(registry[service_name]), service_module))
         all_p += start_service(cwd, service_module, run_daemon, run_ssl)
     return all_p
 
@@ -61,21 +60,35 @@ def start_service(cwd, service_module, run_daemon, run_ssl):
     Starts SNET Daemon ("snetd") and the python module of the service
     at the passed gRPC port.
     """
-
-    def add_ssl_configs(conf):
-        """Add SSL keys to snetd.config.json"""
+    
+    def add_extra_configs(conf):
+        """Add Extra keys to snetd.config.json"""
         with open(conf, "r") as f:
             snetd_configs = json.load(f)
-            snetd_configs["ssl_cert"] = "/opt/singnet/.certs/fullchain.pem"
-            snetd_configs["ssl_key"] = "/opt/singnet/.certs/privkey.pem"
+            if run_ssl:
+                snetd_configs["ssl_cert"] = "/opt/singnet/.certs/fullchain.pem"
+                snetd_configs["ssl_key"] = "/opt/singnet/.certs/privkey.pem"
+                snetd_configs["payment_channel_ca_path"] = "/opt/singnet/.certs/ca.pem"
+                snetd_configs["payment_channel_cert_path"] = "/opt/singnet/.certs/client.pem"
+                snetd_configs["payment_channel_key_path"] = "/opt/singnet/.certs/client-key.pem"
+            _network = "mainnet"
+            if "ropsten" in conf:
+                _network = "ropsten"
+            infura_key = os.environ.get("INFURA_API_KEY", "")
+            if infura_key:
+                snetd_configs["ethereum_json_rpc_endpoint"] = "https://{}.infura.io/{}".format(_network, infura_key)
+            pk_metering = os.environ.get("PVT_KEY_FOR_METERING", "")
+            if pk_metering:
+                snetd_configs["metering_enabled"] = True
+                snetd_configs["metering_end_point"] = "https://marketplace-mt-v2.singularitynet.io"
+                snetd_configs["pvt_key_for_metering"] = pk_metering
         with open(conf, "w") as f:
             json.dump(snetd_configs, f, sort_keys=True, indent=4)
 
     all_p = []
     if run_daemon:
-        for idx, config_file in enumerate(glob.glob("./snetd_configs/*.json")):
-            if run_ssl:
-                add_ssl_configs(config_file)
+        for _, config_file in enumerate(glob.glob("./snetd_configs/*.json")):
+            add_extra_configs(config_file)
             all_p.append(start_snetd(str(cwd), config_file))
     service_name = service_module.split(".")[-1]
     grpc_port = registry[service_name]["grpc"]
